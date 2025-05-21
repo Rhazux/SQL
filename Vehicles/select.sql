714822233cb956fd82decb2330a693a8abc4e52f
Rhazux <163984373+Rhazux@users.noreply.github.com>
2025-05-21 14:46:25 +0200

    Update select.sql

--- Vehicles/select.sql
+++ Vehicles/select.sql
@@ -1,14 +1,14 @@
--- Selekce všech vlastníků z tabulky owners seřazených vzestupně podle příjmení.
+-- Selection of all owners from the owners table sorted in ascending order by last name.
 SELECT * FROM owners ORDER BY last_name ASC;
 
--- Selekce všech vlastníků z tabulky owners, kteří se narodili po roce 1980.
+-- Selection of all owners from the owners table who were born after the year 1980.
 SELECT * FROM owners WHERE birthday > '1980-12-31';
 
--- Selekce všech vlastníků z tabulky owners, kteří pocházejí z města, jehož název začíná písmenem P.
+-- Selection of all owners from the owners table who come from a city whose name starts with the letter P.
 SELECT * FROM owners WHERE city LIKE 'P%';
 
--- Selekce počtu všech jedinečných modelů (DISTINCT) z tabulky models.
+-- Selection of the count of all unique models (DISTINCT) from the models table.
 SELECT COUNT(DISTINCT model_name) AS unique_model_count FROM models;
 
--- Selekce všech VIN kódů (projekce) z tabulky vehicles.
+-- Selection of all VIN codes (projection) from the vehicles table.
 SELECT vin_code FROM vehicles;
